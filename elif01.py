cea1d5c6182322c5d6ec4e4c5af2d1006c409277
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-06-02 22:52:24 +0500

    tayyor

--- elif01.py
+++ elif01.py
@@ -1,1 +1,8 @@
+harorat = int(input("haroratni kiriting: "))
 
+if harorat<=0:
+    print("juda sovuq! Issiq kiyim kiying.")
+if harorat>0 and harorat<=14:
+    print("sovuq kurtka kiying.")
+if harorat>=15:
+    print("ob havo yaxshi")        
--- if03.py
+++ if03.py
@@ -1,1 +1,8 @@
+number = int(input("sonni kiriting: "))
 
+if number % 2 == 0 :
+    print(f"{number} soni 2ga bolinadi")
+if number % 3 == 0 :
+    print(f"{number} soni 3ga bolinadi")    
+if number % 5 == 0 :
+    print(f"{number} soni 5ga bolinadi")    
