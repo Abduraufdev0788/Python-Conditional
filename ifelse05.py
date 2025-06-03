022f184c085939d010ceaeb3b214ab8b26085a6b
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-06-03 14:56:33 +0500

    if else tayyor

--- ifelse05.py
+++ ifelse05.py
@@ -1,1 +1,8 @@
+gmail = input("emailingizni kiriting: ")
 
+if "@" in gmail and ( ".com" == gmail[-4:] or ".uz" == gmail[-3:] or ".net" == gmail[-4:] or ".org" == gmail[-4]):
+    print("emailingiz qabul qilindi")
+else :
+    print("emailingiz notogri formatda")
+    
+    
